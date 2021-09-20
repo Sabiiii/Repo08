dab979ea86648dc6ef28c87152e6f4e369f5b43f
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-21 00:34:55 +0400

    Adding returning boolean values

---
+++ ex09/src/index.js
@@ -0,0 +1,23 @@
+
+function isLess(a, b) {
+
+if (a < b) {
+
+    return true;
+
+} else {
+
+    return false;
+
+}
+
+}
+
+isLess(10, 15);
+isLess(15, 10);
+
+
+console.log(isLess(a, b));
+console.log(isLess(a, b));
+
+module.exports
