3aca3671a1b7b8225a33254ba9e7c695da2dca21
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-21 02:04:04 +0400

    Adding generate random whole numbers

---
+++ ex12/src/index.js
@@ -0,0 +1,13 @@
+
+var randomNumberBetween0and29 = Math.floor(Math.random() * 20);
+
+function randomInteger() {
+
+    var randomNumberBetween0and29 = Math.floor(Math.random() * 20);
+    return Math.floor(Math.random() * 20);
+
+
+}
+
+
+module.exports = randomInteger;
