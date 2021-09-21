5924049c95f85b7f2a11c9de6adcadeeed2341c5
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-21 08:49:05 +0400

    Adding fractions

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -1,23 +1,20 @@
 
 function mathTest(x, y) {
-    
-    console.log(mathTest(2, 2));
-    console.log(mathTest(2, 2));
-    console.log(mathTest(-2, 2));
-    console.log(mathTest(2, -2));
-    console.log(mathTest(2, 8));
-    console.log(mathTest(3, 3));
-    console.log(mathTest(0, 0));
-    
-    if (x < 0 || y < 0) {
 
+    
+    if (x < 0 || y < 0) { 
         return undefined;
-    } else { 
-        return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
-        }
+    }
 
+    return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
 }
 
-mathTest(x, y);
+mathTest(2, 2);
+mathTest(2, 2);
+mathTest(-2, 2);
+mathTest(2, -2);
+mathTest(2, 8);
+mathTest(3, 3);
+mathTest(0, 0);
 
 module.exports = mathTest;
