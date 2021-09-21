788536a02ec149b16263f2bb2dcf240f97bb3516
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-21 10:47:51 +0400

    Adding whoole numbers with range v3

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,10 +1,14 @@
 
-function randomRangeNumber(minNumber, maxNumber) {
+function randomRangeNumber(maxNumber, minNumber) {
+
+    Math.floor(Math.random() * (maxNumber - minNumber) + 1);
 
     return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 
 }
 
 randomRangeNumber(1, 10);
+
+console.log(randomRangeNumber(minNumber, maxNumber));
 
 module.exports = randomRangeNumber;
